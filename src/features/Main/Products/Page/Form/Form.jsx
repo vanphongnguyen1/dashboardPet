a52f16ddd16a9489d37243c1aaaadf066a40bd8a
vanphongnguyen1 <61622503+vanphongnguyen1@users.noreply.github.com>
2021-04-06 23:56:24 +0700

    tabs ui creat product aplly lirbery Ckeditor

--- src/features/Main/Products/Page/Form/Form.jsx
+++ src/features/Main/Products/Page/Form/Form.jsx
@@ -1,14 +1,18 @@
-// import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 // import { customAxiosApi } from '../../../../../customAxiosApi'
-import { HeadingBox } from '../../../../../Components/HeadingBox'
 import GroupInput from '../../../../../Components/Form/GroupInput'
 import { Selector } from '../../../../../Components/Form/Selector'
 import { Textarea } from '../../../../../Components/Form/Textarea'
 import { Lable } from '../../../../../Components/Form/Lable'
 import { Delete, Save } from '../../../../../Components/Btn'
 import DelayLink from '../../../../../Components/DelayLink'
+import { Tabs } from 'antd'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 function Form() {
+  const { TabPane } = Tabs
+  const [dataProduct, setDataProduct] = useState({})
   // const [data, setData] = useState([])
 
   // useEffect(() => {
@@ -21,132 +25,175 @@ function Form() {
 
   // const newUrls = JSON.parse(data.url)
 
+  const handleOnchange = e => {
+    const { value, name } = e.target
+
+    setDataProduct({
+      ...dataProduct,
+      files: value
+    })
+
+    console.log(value)
+  }
+
   return (
     <>
-      <HeadingBox title="Add Product"/>
       <form className="form">
-        <div className="form__product">
-          <GroupInput
-            type="text"
-            name="name"
-            value=""
-            titleLabel="Name Product"
-          />
-        </div>
+        <Tabs defaultActiveKey="1" centered>
+          <TabPane tab="INFOMATION" key="1">
+            <div className="box-tabs">
+              <div className="form__product">
+                <GroupInput
+                  type="text"
+                  name="name"
+                  value=""
+                  titleLabel="Name Product"
+                />
+              </div>
 
-        <div className="form__product">
-          <div className="box-row">
-            <div className="box-6">
-              <GroupInput
-                type="number"
-                name="price"
-                value=""
-                titleLabel="Price"
-              />
+              <div className="form__product">
+                <div className="box-row">
+                  <div className="box-6">
+                    <GroupInput
+                      type="number"
+                      name="price"
+                      value=""
+                      titleLabel="Price"
+                    />
+                  </div>
+
+                  <div className="box-6">
+                    <GroupInput
+                      type="number"
+                      value=""
+                      name="priceSale"
+                      titleLabel="Price Sale"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="form__product">
+                <div className="box-row">
+                  <div className="box-6">
+                    <Selector
+                      name="group"
+                      value=""
+                      title="Group"
+                    />
+                  </div>
+
+                  <div className="box-6">
+                    <Selector
+                      name="lineage"
+                      value=""
+                      title="Lineage"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="form__product">
+                <div className="form__product-box">
+                  <div className="box-checkbox">
+                    <input type="checkbox" className="group__checkbox" id="onProduct"/>
+                    <Lable
+                      htmlFor="onProduct"
+                      text="on"
+                      className="group__checkbox--title"
+                    />
+                  </div>
+
+                  <div className="box-checkbox">
+                    <input type="checkbox" className="group__checkbox" id="offProduct"/>
+                    <Lable
+                      htmlFor="offProduct"
+                      text="off"
+                      className="group__checkbox--title"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="form__product">
+                <div className="form__product-box">
+                  <div className="box-radio">
+                    <input type="radio" className="group__radio" id="newProduct"/>
+                    <Lable
+                      htmlFor="newProduct"
+                      text="New Product"
+                      className="group__radio--title"
+                    />
+                  </div>
+
+                  <div className="box-radio">
+                    <input type="radio" className="group__radio" id="hotProduct"/>
+                    <Lable
+                      htmlFor="hotProduct"
+                      text="Hot Product"
+                      className="group__radio--title"
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
+          </TabPane>
 
-            <div className="box-6">
-              <GroupInput
-                type="number"
-                value=""
-                name="priceSale"
-                titleLabel="Price Sale"
-              />
+          <TabPane tab="IMAGES" key="2">
+            <div className="box-tabs">
+              <div className="form__product">
+                <div className="box-checkbox">
+                  <div className="box-file">
+                    <img src="fakepath/xetrang.jpg" alt="" className="box-file__image"/>
+
+                    <input
+                      type="file"
+                      className="group__files"
+                      id="files"
+                      onChange={handleOnchange}
+                    />
+
+                    <label
+                      htmlFor="files"
+                      text=""
+                      className="group__checkbox--title label-file"
+                    >
+                      <span className="label-file__icon fas fa-plus" />
+                      <span className="label-file__text">
+                        Upload
+                      </span>
+                    </label>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </TabPane>
 
-        <div className="form__product">
-          <div className="box-row">
-            <div className="box-6">
-              <Selector
-                name="group"
-                value=""
-                title="Group"
-              />
+          <TabPane tab="DESCRIPTION" key="3">
+            <div className="box-tabs">
+              <div className="form__product">
+                <CKEditor
+                  editor={ ClassicEditor }
+                  data="<p>Hello from CKEditor 5!</p>"
+                  onReady={ editor => {
+                      // You can store the "editor" and use when it is needed.
+                      console.log( 'Editor is ready to use!', editor );
+                  } }
+                  onChange={ ( event, editor ) => {
+                      // const data = editor.getData();
+                      // console.log( { event, editor, data } );
+                  } }
+                  onBlur={ ( event, editor ) => {
+                      console.log( 'Blur.', editor );
+                  } }
+                  onFocus={ ( event, editor ) => {
+                      console.log( 'Focus.', editor );
+                  } }
+                />
+              </div>
             </div>
-
-            <div className="box-6">
-              <Selector
-                name="lineage"
-                value=""
-                title="Lineage"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="form__product">
-          <div className="box-checkbox">
-              <input type="file" className="group__files" id="files"/>
-
-              <Lable
-                htmlFor="files"
-                text="Images Product"
-                className="group__checkbox--title"
-              />
-          </div>
-        </div>
-
-        <div className="form__product">
-          <div className="form__product-box">
-            <div className="box-checkbox">
-              <input type="checkbox" className="group__checkbox" id="onProduct"/>
-              <Lable
-                htmlFor="onProduct"
-                text="on"
-                className="group__checkbox--title"
-              />
-            </div>
-
-            <div className="box-checkbox">
-              <input type="checkbox" className="group__checkbox" id="offProduct"/>
-              <Lable
-                htmlFor="offProduct"
-                text="off"
-                className="group__checkbox--title"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="form__product">
-          <div className="form__product-box">
-            <div className="box-radio">
-              <input type="radio" className="group__radio" id="newProduct"/>
-              <Lable
-                htmlFor="newProduct"
-                text="New Product"
-                className="group__radio--title"
-              />
-            </div>
-
-            <div className="box-radio">
-              <input type="radio" className="group__radio" id="hotProduct"/>
-              <Lable
-                htmlFor="hotProduct"
-                text="Hot Product"
-                className="group__radio--title"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div className="form__product">
-          <Textarea
-            name="info"
-            value=""
-            title="info"
-          />
-        </div>
-
-        <div className="form__product">
-          <Textarea
-            name="discription"
-            value=""
-            title="Discription"
-          />
-        </div>
+          </TabPane>
+        </Tabs>
 
         <div className="box-submit">
           <div className="box-row">
--- src/features/Main/Products/Page/index.js
+++ src/features/Main/Products/Page/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import EditProduct from './Edit'
 
 const Products = () => {
+  // const data = "<li>aaaaa</li>, <li>aaaaa</li>, <li>aaaaa</li>, <li>aaaaa</li>"
+  // const newData = data.split(',')
   return (
     <div>
       <EditProduct />
+      {/* { newData.map(item => {
+        return <div className="editor" dangerouslySetInnerHTML={{__html:item}}/>
+      })} */}
     </div>
   )
 }
